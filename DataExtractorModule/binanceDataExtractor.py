f277637f462ffaafafe5fca6e9d0f5f7f09d9b48
sosooding <rishiksood15@gmail.com>
2025-05-23 14:29:46 +1000

    Enabled batches to remove 1000 line limit

--- DataExtractorModule/binanceDataExtractor.py
+++ DataExtractorModule/binanceDataExtractor.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import pandas as pd
 import yaml
 import os.path
@@ -14,14 +15,37 @@ class BinanceDataExtractor(DataExtractor):
 			api_secret = cfg['binance']['api_secret']
 		self.client = Client(api_key = api_key, api_secret = api_secret)
 
-	def extractSymbolBetweenDates(self, symbol, startDate, endDate):
-		filename = f'data\\{symbol}_{startDate}_{endDate}.csv'
+	def extractSymbolBetweenDates(self, symbol: str, startDate: datetime, endDate: datetime):
+		filename = f'data\\{symbol}_{startDate.strftime('%Y%m%d_%H%M%S')}_{endDate.strftime('%Y%m%d_%H%M%S')}.csv'
 
 		# Cache check
 		if os.path.isfile(filename):
 			return pd.read_csv(filename)
 
-		klines = self.client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, startDate.strftime("%d %b %Y %H:%M:%S"), endDate.strftime("%d %b %Y %H:%M:%S"), 1000)
+		klines = []
+		current_start = startDate
+
+		while current_start < endDate:
+			batch_end = min(
+				current_start + timedelta(minutes=1000),
+				endDate
+			)
+
+			batch = self.client.get_historical_klines(
+				symbol 		= symbol,
+				interval 	= Client.KLINE_INTERVAL_1MINUTE,
+				start_str 	= current_start.strftime("%d %b %Y %H:%M:%S"),
+				end_str 	= batch_end.strftime("%d %b %Y %H:%M:%S"),
+				limit 		= 1000
+			)
+        
+			if batch:
+				klines.extend(batch)
+				last_timestamp = batch[-1][0]
+				current_start = datetime.fromtimestamp(last_timestamp/1000) + timedelta(minutes=1)
+			else:
+				break
+		
 		data = pd.DataFrame(klines, columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore' ])
 		data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
 
